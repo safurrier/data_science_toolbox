c2be0bd91a21bcb576e8f96e1a7043754932fee6
safurrier <safurrier@gmail.com>
2019-12-06 13:16:10 -0700

    Parameterized tests.io.excel_to_df_dict.py for tests and added a second test case to it

--- tests/io/test_file_search_and_replace.py
+++ tests/io/test_file_search_and_replace.py
@@ -3,56 +3,74 @@ import os
 import shutil
 import pathlib
 import pandas as pd
+import pytest
 from data_science_toolbox.io.file_search_and_replace import file_search_and_replace
 
-DATA_FPATH = 'tests/io/data/test_df_dict.xlsx'
-TEST_ALL_SHEETS = ['one', 'two', 'three']
-TEST_SHEETS_INCLUDE = ['one', 'three']
-TEST_SHEETS_EXCLUDE = ['two']
-
-#########
-# WARNING
-#########
-
-# The directory specified by TEST_DIR_CREATE
+# The directory specified by 'create_dir'
 # Will be created and destroyed.
 # Do not set it to any directory containing files
 # you do not wish deleted
+# Note: 'create_dir' is the first item in the test case arg list
 
-TEST_DIR_CREATE = pathlib.Path('tests/io/search_and_replace_test_files')
+# Form of individual test case is
+# (create_dir,
+# test_search_fnames,
+# test_replaced_fnames,
+# search_phrase,
+# replace_phrase)
+
+FILE_SEARCH_AND_REPLACE_TEST_CASES = [
+    ('tests/io/search_and_replace_test_files',
+     ['incorrect_fname_1.txt',
+      'incorrect_fname_2.txt',
+      'correct_fname_3.txt'],
+     ['correct_fname_1.txt',
+      'correct_fname_2.txt',
+      'correct_fname_3.txt'],
+     'incorrect',
+     'correct',
+     ),
+    ('tests/io/search_and_replace_test_files',
+     ['broken_fname_1.txt',
+      'brokenfname_2.txt',
+      'fixed3.txt'],
+     ['fixed_fname_1.txt',
+      'fixedfname_2.txt',
+      'fixed3.txt'],
+     'broken',
+     'fixed',
+     ),
+]
 
 
-TEST_SEARCH_FNAMES = ['incorrect_fname_1.txt',
-                      'incorrect_fname_2.txt',
-                      'correct_fname_3.txt']
-
-TEST_REPLACED_FNAMES = ['correct_fname_1.txt',
-                        'correct_fname_2.txt',
-                        'correct_fname_3.txt']
-
-TEST_SEARCH_PHRASE = 'incorrect'
-TEST_REPLACE_PHRASE = 'correct'
-
-
-def test_file_search_and_replace():
+@pytest.mark.parametrize("""create_dir,
+                         test_search_fnames, test_replaced_fnames,
+                         search_phrase, replace_phrase""",
+                         FILE_SEARCH_AND_REPLACE_TEST_CASES
+                         )
+def test_file_search_and_replace(create_dir,
+                                 test_search_fnames, test_replaced_fnames,
+                                 search_phrase, replace_phrase):
+    # pathlib.Path the create directory
+    create_dir = pathlib.Path(create_dir)
     # Create test dir
-    if not os.path.exists(TEST_DIR_CREATE.as_posix()):
-        os.mkdir(TEST_DIR_CREATE.as_posix())
+    if not os.path.exists(create_dir.as_posix()):
+        os.mkdir(create_dir.as_posix())
     # Create test filenames
-    for test_file in TEST_SEARCH_FNAMES:
-        if not os.path.exists(TEST_DIR_CREATE / test_file):
-            with open(TEST_DIR_CREATE / test_file, 'w'):
+    for test_file in test_search_fnames:
+        if not os.path.exists(create_dir / test_file):
+            with open(create_dir / test_file, 'w'):
                 pass
 
     # Search and replace
-    file_search_and_replace(TEST_DIR_CREATE.as_posix(),
-                            TEST_SEARCH_PHRASE,
-                            TEST_REPLACE_PHRASE)
+    file_search_and_replace(create_dir.as_posix(),
+                            search_phrase,
+                            replace_phrase)
 
     # Get filenames
-    replaced_fnames = os.listdir(TEST_DIR_CREATE.as_posix())
+    replaced_fnames = os.listdir(create_dir.as_posix())
     # Remove directory and files
-    shutil.rmtree(TEST_DIR_CREATE.as_posix())
+    shutil.rmtree(create_dir.as_posix())
 
     # Assert have been correctly replaced
-    assert not set(TEST_REPLACED_FNAMES).difference(replaced_fnames)
+    assert not set(test_replaced_fnames).difference(replaced_fnames)
