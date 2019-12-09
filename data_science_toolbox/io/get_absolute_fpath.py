7a7f6a194befc4ccb89d37602b674ea8469a7213
safurrier <safurrier@gmail.com>
2019-12-09 09:43:58 -0700

    Refactored io.get_absolute_fpath.py

--- data_science_toolbox/io/get_absolute_fpath.py
+++ data_science_toolbox/io/get_absolute_fpath.py
@@ -1,9 +1,40 @@
 import os
 import warnings
 
-def get_absolute_fpath(target_fname: str = 'README.md', levels_to_check: int = 10, verbose=0):
+
+def get_absolute_fpath(target_fname: str = 'README.md', levels_to_check: int = 10, verbose: int = 0) -> str:
     """Pass a filename that exists in a directory an unknown number of
-    levels higher. Return the string absolute path of the file
+    levels higher. Return the string absolute path of said file.
+
+    Parameters
+    ----------
+    target_fname : str, default = 'README.md'
+        Filename to search for in parent directories.
+        Default is a markdown file README
+    levels_to_check : int
+        The number of parent def
+    verbose : int, optional
+        An verbosity flag to print out the absolute fpath 
+        found
+
+    Returns
+    -------
+    target_dir: str
+        The string absolute filepath to 'target_fname' 
+
+    Example
+    -------
+
+    Example Directory:
+    + sample_dir
+        + target_file.txt
+        + sub_sample_dir
+            + sub_sub_sample_dir
+            (current directory)
+
+    target_fpath = get_absolute_fpath('target_file.txt')
+    > target_fpath
+    > C://users/projects/example_dir/sample_dir/target_file.txt
     """
     original_wd = os.getcwd()
     for x in range(0, levels_to_check):
@@ -13,13 +44,14 @@ def get_absolute_fpath(target_fname: str = 'README.md', levels_to_check: int = 1
             if verbose:
                 warnings.warn(
                     f"""\n\nUnable to find directory with file {target_fname} within {levels_to_check} parent directories""")
-            return
+            target_dir = None
+            break
         # Check if README exists
-        if os.path.isfile(target_fname):
+        elif os.path.isfile(target_fname):
             target_dir = os.getcwd()
             if verbose:
                 print(f'Found target file in {target_dir}')
-            return target_dir
         # If not found move back one directory level
         else:
             os.chdir('../')
+    return target_dir
--- data_science_toolbox/io/read_data.py
+++ data_science_toolbox/io/read_data.py
@@ -2,13 +2,14 @@ import pandas as pd
 import pathlib
 from typing import List
 
-def read_data(fpath: str, columns: List[str]=None, reader_kwargs: dict = {}, ) -> pd.DataFrame:
+
+def read_data(fpath: str, columns: List[str] = None, reader_kwargs: dict = {}, ) -> pd.DataFrame:
     """
     Pass a filepath and have said file read in as a Pandas DataFrame.
     Supports csv, excel, hdf, pickle
     Parameters for reading in added as dict to reader_kwargs param
 
-    
+
     Given a list of column names, check to see if each of those appears
     in the columns of a dataframe filtered by dtype=np.number
 
@@ -20,6 +21,8 @@ def read_data(fpath: str, columns: List[str]=None, reader_kwargs: dict = {}, ) -
         A list of columns to keep
     reader_kwargs : dict
         An optional dictionary of pandas reader kwargs
+
+    Return
     -------
     DataFrame
         A pandas DataFrame read in from the filepath
@@ -38,9 +41,10 @@ def read_data(fpath: str, columns: List[str]=None, reader_kwargs: dict = {}, ) -
     fpath = pathlib.Path(fpath)
     file_format = fpath.suffix
     if file_format not in ['.csv', '.pickle', '.pkl', '.xlsx', '.hdf']:
-        print(f'File format "{file_format}" does not appear to be a supported file format')
+        print(
+            f'File format "{file_format}" does not appear to be a supported file format')
         raise ValueError
-    
+
     # Determine input file type and pandas reader
     if file_format == '.csv':
         reader = pd.read_csv
@@ -49,30 +53,32 @@ def read_data(fpath: str, columns: List[str]=None, reader_kwargs: dict = {}, ) -
     elif (file_format == '.xlsx'):
         reader = pd.read_excel
     elif (file_format == '.hdf'):
-        reader = pd.read_hdf        
+        reader = pd.read_hdf
 
     # Read in filepath:
     try:
         data = reader(fpath.as_posix(), **reader_kwargs)
     except FileNotFoundError as e:
-        print(f'Unable to read in {fpath.as_posix()}. Check the file_format and file path')
+        print(
+            f'Unable to read in {fpath.as_posix()}. Check the file_format and file path')
         raise e
-    
+
     missing_cols = []
     # If specificed, look for subset of columns
     if columns:
         columns = [col
-                     if col in data.columns.values.tolist()
-                     else missing_cols.append(col)
-                     for col
-                     in columns]
+                   if col in data.columns.values.tolist()
+                   else missing_cols.append(col)
+                   for col
+                   in columns]
     else:
         columns = data.columns.values.tolist()
-    
-    assert columns != [None], 'No columns from columns param found in data' 
+
+    assert columns != [None], 'No columns from columns param found in data'
     # Print missing columns if present
     if missing_cols:
-        print(f'WARNING: These columns were not found in the data: {missing_cols}')
+        print(
+            f'WARNING: These columns were not found in the data: {missing_cols}')
 
     # Subset to desired columns
     data = data[columns]
